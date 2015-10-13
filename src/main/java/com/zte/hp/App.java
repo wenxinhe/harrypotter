4d005fcfe330bc6e4629c1ba6965509291f1e094
vincent <wenxin.he@g>
2015-10-13 19:53:30 +0800

    init

---
+++ src/main/java/com/zte/hp/App.java
@@ -0,0 +1,13 @@
+package com.zte.hp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
