ba4a18fd6d0e2a538f3ae9f7c7932e5813cb6dd4
vincent <wenxin.he@g>
2015-10-13 20:19:15 +0800

    first test case pass

--- src/main/java/com/zte/hp/Book.java
+++ src/main/java/com/zte/hp/Book.java
@@ -3,7 +3,14 @@ package com.zte.hp;
  *  vincent on 15-10-13.
  */
 public class Book {
-    public Book(Volume volume) {
 
+    private Volume volume;
+
+    public Book(Volume volume) {
+        this.volume = volume;
+    }
+
+    public double getPrice() {
+        return 32.0;
     }
 }
--- src/main/java/com/zte/hp/Cart.java
+++ src/main/java/com/zte/hp/Cart.java
@@ -1,10 +1,13 @@
 package com.zte.hp;
 
 public class Cart {
+    private Book book;
+
     public Cart(Book book) {
+        this.book = book;
     }
 
     public double cost() {
-        return 0;
+        return book.getPrice();
     }
 }
